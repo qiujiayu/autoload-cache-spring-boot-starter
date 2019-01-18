87fea203c7d70e88491c9f21014383bb38e10e11
qiujiayu <153163285@qq.com>
2019-01-18 14:45:06 +0800

    7.0.2-1版本发布，增加Lettuce的自动配置

--- src/main/java/com/jarvis/cache/autoconfigure/AutoloadCacheManageConfiguration.java
+++ src/main/java/com/jarvis/cache/autoconfigure/AutoloadCacheManageConfiguration.java
@@ -3,6 +3,7 @@ package com.jarvis.cache.autoconfigure;
 import com.jarvis.cache.ICacheManager;
 import com.jarvis.cache.redis.AbstractRedisCacheManager;
 import com.jarvis.cache.redis.JedisClusterCacheManager;
+import com.jarvis.cache.redis.LettuceRedisClusterCacheManager;
 import com.jarvis.cache.redis.SpringRedisCacheManager;
 import com.jarvis.cache.script.AbstractScriptParser;
 import com.jarvis.cache.script.OgnlParser;
@@ -11,21 +12,29 @@ import com.jarvis.cache.serializer.HessianSerializer;
 import com.jarvis.cache.serializer.ISerializer;
 import com.jarvis.cache.serializer.JdkSerializer;
 import com.jarvis.cache.serializer.KryoSerializer;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.cluster.RedisClusterClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClusterConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.util.ClassUtils;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+
+import java.lang.reflect.Field;
 
 /**
  * 对autoload-cache进行一些默认配置<br>
@@ -71,7 +80,8 @@ public class AutoloadCacheManageConfiguration {
     @ConditionalOnMissingBean(ISerializer.class)
     public ISerializer<Object> autoloadCacheSerializer() {
         ISerializer<Object> res;
-        if (hessianPresent) {// 推荐优先使用：Hessian
+        // 推荐优先使用：Hessian
+        if (hessianPresent) {
             res = new HessianSerializer();
             log.debug("HessianSerializer auto-configured");
         } else if (kryoPresent) {
@@ -84,50 +94,98 @@ public class AutoloadCacheManageConfiguration {
         return res;
     }
 
-    /**
-     * 默认只支持{@link JedisClusterCacheManager JedisClusterCacheManager}<br>
-     *
-     * @param config
-     * @param serializer
-     * @param applicationContext
-     * @return
-     */
-    @Bean
-    @ConditionalOnMissingBean(ICacheManager.class)
-    @ConditionalOnClass(name = "org.springframework.data.redis.connection.RedisConnectionFactory")
-    public ICacheManager autoloadCacheCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer,
-                                                   ApplicationContext applicationContext) {
-        return createRedisCacheManager(config, serializer, applicationContext);
-    }
-
-    public static ICacheManager createRedisCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer, ApplicationContext applicationContext) {
-        RedisConnectionFactory connectionFactory = null;
-        try {
-            connectionFactory = applicationContext.getBean(RedisConnectionFactory.class);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-        if (null == connectionFactory) {
-            return null;
+    @Configuration
+    @ConditionalOnClass(Jedis.class)
+    static class JedisCacheCacheManagerConfiguration {
+        /**
+         * 默认只支持{@link JedisClusterCacheManager}<br>
+         *
+         * @param config
+         * @param serializer
+         * @param connectionFactory
+         * @return
+         */
+        @Bean
+        @ConditionalOnMissingBean(ICacheManager.class)
+        @ConditionalOnBean(JedisConnectionFactory.class)
+        public ICacheManager autoloadCacheCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer,
+                                                       JedisConnectionFactory connectionFactory) {
+            return createRedisCacheManager(config, serializer, connectionFactory);
         }
 
-        RedisConnection redisConnection = null;
-        try {
-            redisConnection = connectionFactory.getConnection();
-        } catch (Throwable e) {
-            log.error(e.getMessage(), e);
+        private ICacheManager createRedisCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer, JedisConnectionFactory connectionFactory) {
+            RedisConnection redisConnection = null;
+            try {
+                redisConnection = connectionFactory.getConnection();
+                AbstractRedisCacheManager cacheManager = null;
+                if (redisConnection instanceof JedisClusterConnection) {
+                    JedisClusterConnection redisClusterConnection = (JedisClusterConnection) redisConnection;
+                    // 优先使用JedisCluster; 因为JedisClusterConnection 批量处理，需要使用JedisCluster
+                    JedisCluster jedisCluster = redisClusterConnection.getNativeConnection();
+                    cacheManager = new JedisClusterCacheManager(jedisCluster, serializer);
+                } else {
+                    cacheManager = new SpringRedisCacheManager(connectionFactory, serializer);
+                }
+                // 根据需要自行配置
+                cacheManager.setHashExpire(config.getJedis().getHashExpire());
+                return cacheManager;
+            } catch (Throwable e) {
+                log.error(e.getMessage(), e);
+                throw e;
+            } finally {
+                RedisConnectionUtils.releaseConnection(redisConnection, connectionFactory);
+            }
         }
-        AbstractRedisCacheManager cacheManager;
-        if (redisConnection instanceof JedisClusterConnection) {
-            JedisClusterConnection redisClusterConnection = (JedisClusterConnection) redisConnection;
-            // 优先使用JedisCluster; 因为JedisClusterConnection 批量处理，需要使用JedisCluster
-            JedisCluster jedisCluster = redisClusterConnection.getNativeConnection();
-            cacheManager = new JedisClusterCacheManager(jedisCluster, serializer);
-        } else {
-            cacheManager = new SpringRedisCacheManager(connectionFactory, serializer);
-        }
-        // 根据需要自行配置
-        cacheManager.setHashExpire(config.getJedis().getHashExpire());
-        return cacheManager;
     }
+
+    @Configuration
+    @ConditionalOnClass(RedisClient.class)
+    static class LettuceCacheCacheManagerConfiguration {
+        /**
+         * 默认只支持{@link LettuceRedisClusterCacheManager}<br>
+         *
+         * @param config
+         * @param serializer
+         * @param connectionFactory
+         * @return
+         */
+        @Bean
+        @ConditionalOnMissingBean(ICacheManager.class)
+        @ConditionalOnBean(LettuceConnectionFactory.class)
+        public ICacheManager autoloadCacheCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer,
+                                                       LettuceConnectionFactory connectionFactory) {
+            return createRedisCacheManager(config, serializer, connectionFactory);
+        }
+
+        private ICacheManager createRedisCacheManager(AutoloadCacheProperties config, ISerializer<Object> serializer, LettuceConnectionFactory connectionFactory) {
+            RedisConnection redisConnection = null;
+            try {
+                redisConnection = connectionFactory.getConnection();
+                AbstractRedisCacheManager cacheManager = null;
+                if (redisConnection instanceof LettuceClusterConnection) {
+                    LettuceClusterConnection lettuceClusterConnection = (LettuceClusterConnection) redisConnection;
+                    try {
+                        Field clusterClientField = LettuceClusterConnection.class.getDeclaredField("clusterClient");
+                        clusterClientField.setAccessible(true);
+                        RedisClusterClient redisClusterClient = (RedisClusterClient) clusterClientField.get(lettuceClusterConnection);
+                        cacheManager = new LettuceRedisClusterCacheManager(redisClusterClient, serializer);
+                    } catch (Exception e) {
+                        log.error(e.getMessage(), e);
+                    }
+                } else {
+                    cacheManager = new SpringRedisCacheManager(connectionFactory, serializer);
+                }
+                // 根据需要自行配置
+                cacheManager.setHashExpire(config.getJedis().getHashExpire());
+                return cacheManager;
+            } catch (Throwable e) {
+                log.error(e.getMessage(), e);
+                throw e;
+            } finally {
+                RedisConnectionUtils.releaseConnection(redisConnection, connectionFactory);
+            }
+        }
+    }
+
+
 }
